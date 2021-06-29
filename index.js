95c8deb2d5040c96dea4fea757181402c0a65c87
jesus <chucho.cisneros@hotmail.com>
2021-06-29 16:26:32 -0500

    prueba4
    cambio conts por var

--- index.js
+++ index.js
@@ -1,49 +1,42 @@
-import express from 'express';
-import router from './routes/index.js';
-import db from './config/db.js';
-import dotenv from 'dotenv';
-dotenv.config({ path: 'variables.env' });
-const app=express();
+import express from "express";
+import router from "./routes/index.js";
+import db from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "variables.env" });
+const app = express();
 
 //Conectar la base de datos
 db.authenticate()
-    .then( () =>console.log('Base de datos conectada'))
-    .catch( error => console.log(error));
+  .then(() => console.log("Base de datos conectada"))
+  .catch((error) => console.log(error));
 
 //Definir puerto
 //variables de entorno process.env.PORT
-const host=process.env.HOST || '0.0.0.0';
-const port=process.env.PORT || 4000;
-
-
+var host = process.env.HOST || "0.0.0.0";
+var port = process.env.PORT || 4000;
 
 //HABILITAR PUG
-app.set('view engine','pug');
-
+app.set("view engine", "pug");
 
 //Obtener el año actual
-app.use( (req, res, next) => {
-    const year = new Date();
-    res.locals.actualYear = year.getFullYear();
-    
-    
-    //console.log(req);
-     next();
-});
+app.use((req, res, next) => {
+  const year = new Date();
+  res.locals.actualYear = year.getFullYear();
 
+  //console.log(req);
+  next();
+});
 
 // Agregar body parser para leer los datos del formulario
 
-
-app.use(express.urlencoded({extended:true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 //Definir la carpeta publica
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //agregar router
-app.use('/',router);
+app.use("/", router);
 
-app.listen(port,host,()=>{
-    console.log(`El servidor esta funcionando en el puerto ${port}`)
-})
+app.listen(port, host, () => {
+  console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
